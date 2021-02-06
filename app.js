027bfb5f2e3797ad1810c82fa0461c9eb0e492f5
Peter Oettig <peet1993@gmail.com>
2021-02-06 18:29:12 +0100

    Changed reporting channel for SRC changes.

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ function fetchAllFromSRCURL(pageUrl, earliest_submission_timestamp = 0, data = [
 async function checkSRC() {
     const INTERVAL = 60 * 60 * 1000; // Each hour.
     const URL = "https://www.speedrun.com/api/v1/runs?game=9d35xw1l&orderby=date&direction=desc&embed=category,players";
-    const TARGET_CHANNEL = "⛔moderator-chat";
+    const TARGET_CHANNEL = "🏢speedrun-administration";
     const GAME_ID = "9d35xw1l";
 
     db("src").select().orderBy("internal_id", "desc").first().then((row) => {
@@ -248,6 +248,9 @@ async function checkSRC() {
 
         });
     }).finally(() => {
+        setTimeout(() => {
+            Discord.sendMessage("Hello, I'm reporting here now.", TARGET_CHANNEL);
+        }, 2000);
         return Promise.delay(INTERVAL).then(() => checkSRC());
     });
 }
