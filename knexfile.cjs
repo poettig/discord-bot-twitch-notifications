db27d75657a122dd38af89576dee5ecc8763d277
Peter Oettig <dev@oettig.de>
2024-11-10 17:22:34 +0100

    fix: knexfile used hardcoded db file path

--- knexfile.cjs
+++ knexfile.cjs
@@ -1,10 +1,11 @@
 'use strict';
 
+const config = require("./config.json")
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './db.sqlite3'
+      filename: config.store.connection
     },
     useNullAsDefault: true,
   },
